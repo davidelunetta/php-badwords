fd4a74464a20a3ec822df5e0df85d4fa35729266
davidelunetta <davidelunetta.27@gmail.com>
2023-11-27 14:39:33 +0100

    added form control

--- form_control.php
+++ form_control.php
@@ -6,6 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+
+$paragrafo = $_POST["paragrafo"];
+$parolaCensurare = $_POST["parola_censurare"];
+
+// Stampa il paragrafo e la sua lunghezza
+echo "<p>Paragrafo inserito: $paragrafo</p>";
+echo "<p>Lunghezza del paragrafo: " . strlen($paragrafo) . " caratteri</p>";
+
+// Sostituisce la parola da censurare con tre asterischi
+$paragrafoCensurato = str_ireplace($parolaCensurare, '***', $paragrafo);
+
+// Stampa il paragrafo censurato e la sua lunghezza
+echo "<p>Paragrafo censurato: $paragrafoCensurato</p>";
+echo "<p>Lunghezza del paragrafo censurato: " . strlen($paragrafoCensurato) . " caratteri</p>";
+?>
 </body>
 </html>
