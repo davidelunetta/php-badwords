0f818b09de40767a8f7f87cdd4b86c104cb6d0d6
davidelunetta <davidelunetta.27@gmail.com>
2023-11-27 14:31:11 +0100

    added form input

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+<form action="form_control.php" method="post">
+        <label for="paragrafo">Inserisci il paragrafo:</label><br>
+        <textarea id="paragrafo" name="paragrafo" rows="4" cols="50"></textarea><br><br>
+        
+        <label for="parola_censurare">Parola da censurare:</label><br>
+        <input type="text" id="parola_censurare" name="parola_censurare"><br><br>
+
+        <input type="submit" value="Invia">
+    </form>
+</body>
+</html>
